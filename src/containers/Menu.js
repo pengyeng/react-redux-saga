b03214f0581c52270b4c93388b2db03a7ee8c2a8
pengyeng <pengyeng@hotmail.com>
2022-05-06 16:14:56 +0100

    data grid implementation
    Commit implementation of data grid
    
    Co-Authored-By: pengyeng <88436752+pengyeng@users.noreply.github.com>

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -6,14 +6,17 @@ import {
   } from "react-router-dom";
 import Home from "./Home";
 import {NewsMenuItem, NewsLink} from './menulink-APP_MODE';
-
 const Weather = React.lazy(() => import('./Weather'));
+const Data = React.lazy(() => import('./Data'));
 
 function HomeLink(){
   return <Route path="/home" component={Home}/>;
 }
 function WeatherLink(){
   return <Route path="/weather" component={Weather}/>;
+}
+function DataLink(){
+  return <Route path="/datagrid" component={Data}/>;
 }
 
 class Menu extends React.Component {
@@ -25,12 +28,14 @@ class Menu extends React.Component {
           <ul className="header">
             <li><NavLink to="/home">Home</NavLink></li>
             <li><NavLink to="/weather">Weather</NavLink></li>
+            <li><NavLink to="/datagrid">Data Grid</NavLink></li>
             <NewsMenuItem/>
           </ul>
           <div className="content">
           <React.Suspense fallback={<div>loading...</div>}>
             <HomeLink/>
             <WeatherLink/>
+            <DataLink/>
             <NewsLink/>
           </React.Suspense>    
           </div>  
